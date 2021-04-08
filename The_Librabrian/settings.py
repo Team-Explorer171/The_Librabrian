7ca617a89a3f4bc61ad7f959b96eca2d5a30301b
Team-Explorer171 <61952834+Team-Explorer171@users.noreply.github.com>
2021-04-08 15:37:58 +0600

    Update settings.py

--- The_Librabrian/settings.py
+++ The_Librabrian/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'The_Librabrian.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'the_library',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
